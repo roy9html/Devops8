3b21d3fae4a228f878788b2d6470e755ebb32077
Queentel <joiqueentel@gmail.com>
2025-07-17 14:51:06 +0300

    recognize changes

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='home'>
+        <h1>Welcome To My Page!</h1>
+        <p>Hi,my name is Shantell.Feel free to get in touch with me</p>
+
+    </div>
   )
 }
 
